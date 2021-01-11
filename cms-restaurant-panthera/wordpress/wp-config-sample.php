8d06d293791837fe769c7bbc082ccc0b23f67245
Achouffe666 <austin@benard.be>
2021-01-11 13:38:51 +0100

    wp-config update

--- cms-restaurant-panthera/wordpress/wp-config-sample.php
+++ cms-restaurant-panthera/wordpress/wp-config-sample.php
@@ -4,10 +4,10 @@
  *
  * Ce fichier est utilisé par le script de création de wp-config.php pendant
  * le processus d’installation. Vous n’avez pas à utiliser le site web, vous
- * pouvez simplement renommer ce fichier en « wp-config.php » et remplir les
+ * pouvez simplement renommer ce fichier en « wp-config.php » et remplir les
  * valeurs.
  *
- * Ce fichier contient les réglages de configuration suivants :
+ * Ce fichier contient les réglages de configuration suivants :
  *
  * Réglages MySQL
  * Préfixe de table
@@ -31,10 +31,10 @@ define( 'DB_USER', 'team-pantera' );
 define( 'DB_PASSWORD', '4imFB.MedEkv' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', '188.166.24.55/' );
+define( 'DB_HOST', 'http://188.166.24.55/' );
 define('FS_METHOD','direct');
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8' );
 
 /**
  * Type de collation de la base de données.
@@ -53,14 +53,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '5y:4B<qk]]kq+@+JuN{~l`SitzW9q6o631#L>^(oA[,h.+nN{?$S3nZ3s/e|s@Fw' );
-define( 'SECURE_AUTH_KEY',  'j;0MNeb/xmJ} I(xeVpVRe?:o-I~V^J58RuDnD`j%dTUlt)Z)O C5,&-~ihE4>y$' );
-define( 'LOGGED_IN_KEY',    'eA$HlxO{TFk!re9AJYSBvT:HS#=NL3mv`,cB;&qiQsSz>e,MiehNC=S 7d@)X!l$' );
-define( 'NONCE_KEY',        '6CX;._`0A>;4Q!azhFzv`;DE8K_z^%k}u/N*TsJF6nP]+WZ0{]fH6,4aJKlRq4A,' );
-define( 'AUTH_SALT',        'qSYKW#Kp|fW 16#SjA3ZSVY1*/#EP7TFI@j!V33&BmAX/kl)7B%[a~LU3`msv0P%' );
-define( 'SECURE_AUTH_SALT', 'x(hixk=xh-qX?TD]%k4#tTkV cd&c#s>_9/ufjw22,B}kbVb3Qste/M~1OxvgX&Y' );
-define( 'LOGGED_IN_SALT',   'em?j!-(o$Nzo6H$eS1$3F{a An*sFJ!kD6sd9}s=ny5p[0@$7|h4ZfPTKjYkWju{' );
-define( 'NONCE_SALT',       'fC((x4QGL5NMl9te_vz10kVJ}yP(<3:m[>vukr@@Yl1YaOM=G2gkXyFq#.&|`^_C' );
+define( 'AUTH_KEY',         'mettez une phrase unique ici' );
+define( 'SECURE_AUTH_KEY',  'mettez une phrase unique ici' );
+define( 'LOGGED_IN_KEY',    'mettez une phrase unique ici' );
+define( 'NONCE_KEY',        'mettez une phrase unique ici' );
+define( 'AUTH_SALT',        'mettez une phrase unique ici' );
+define( 'SECURE_AUTH_SALT', 'mettez une phrase unique ici' );
+define( 'LOGGED_IN_SALT',   'mettez une phrase unique ici' );
+define( 'NONCE_SALT',       'mettez une phrase unique ici' );
 /**#@-*/
 
 /**
@@ -68,7 +68,7 @@ define( 'NONCE_SALT',       'fC((x4QGL5NMl9te_vz10kVJ}yP(<3:m[>vukr@@Yl1YaOM=G2g
  *
  * Vous pouvez installer plusieurs WordPress sur une seule base de données
  * si vous leur donnez chacune un préfixe unique.
- * N’utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés !
+ * N’utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés !
  */
 $table_prefix = 'wp_';
 
