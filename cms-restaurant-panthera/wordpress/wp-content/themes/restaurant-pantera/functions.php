c7f92aa5e6b53aa55868156e3bbe2b3f3b41f51b
iCarolinei <iCarolinei@github.com>
2021-01-12 11:15:13 +0100

    44

---
+++ cms-restaurant-panthera/wordpress/wp-content/themes/restaurant-pantera/functions.php
@@ -0,0 +1,12 @@
+<?php //Interpreter bootstrap
+function montheme_register_assets () {
+wp_register_style("bootstrap","https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css", []);
+wp_register_script("bootstrap","https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"); //l'order dans lequel il execute ( il a besoin de popper et jquery avant de lancer bootstrap )
+wp_deregister_script("jquerymin"); 
+wp_register_script("jquery","https://code.jquery.com/jquery-3.2.1.slim.min.js", [], false, true);
+wp_enqueue_style("bootstrap");
+wp_enqueue_script("bootstrap");
+}
+
+add_action("wp_enqueue_scripts", "montheme_register_assets");
+
