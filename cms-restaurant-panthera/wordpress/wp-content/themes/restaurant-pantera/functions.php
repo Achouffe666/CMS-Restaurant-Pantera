0a50d9f2c991f2d2fe3430e8048b885cfe1b2f32
iCarolinei <iCarolinei@github.com>
2021-01-12 15:13:57 +0100

    header

--- cms-restaurant-panthera/wordpress/wp-content/themes/restaurant-pantera/functions.php
+++ cms-restaurant-panthera/wordpress/wp-content/themes/restaurant-pantera/functions.php
@@ -1,5 +1,5 @@
-<?php //Interpreter bootstrap
-function montheme_register_assets () {
+<?php
+function pantera_register_assets () {
 wp_register_style("bootstrap","https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css", []);
 wp_register_script("bootstrap","https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"); //l'order dans lequel il execute ( il a besoin de popper et jquery avant de lancer bootstrap )
 wp_deregister_script("jquerymin"); 
@@ -8,5 +8,19 @@ wp_enqueue_style("bootstrap");
 wp_enqueue_script("bootstrap");
 }
 
-add_action("wp_enqueue_scripts", "montheme_register_assets");
+add_action("wp_enqueue_scripts", "pantera_register_assets");
 
+add_theme_support("post-thumbnails");
+add_theme_support("title-tag");
+
+register_nav_menus( array(
+'main' => 'Menu Principal',
+) );
+
+// Déclarer style.css à la racine du thème
+    wp_enqueue_style( 
+        'restaurant-pantera',
+        get_stylesheet_uri(), 
+        array(), 
+        '1.0'
+    );
