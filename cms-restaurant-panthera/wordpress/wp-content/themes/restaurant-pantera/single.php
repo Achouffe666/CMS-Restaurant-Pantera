a70ef67d95b6448432b4551d20d8e4fc2370e71e
iCarolinei <iCarolinei@github.com>
2021-01-18 17:48:28 +0100

    push

--- cms-restaurant-panthera/wordpress/wp-content/themes/restaurant-pantera/single.php
+++ cms-restaurant-panthera/wordpress/wp-content/themes/restaurant-pantera/single.php
@@ -7,23 +7,41 @@
   
 <div class="row">
 <article>
+<nav class="navbar navbar-expand-md ">
+<div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class=" navbar-nav mr-auto">
+            <li class="border-right d-flex align-items-center nav-item active">
+            <i class="fas fa-arrow-left"></i>                <a class="nav-link" href="<?php echo home_url( '/' ); ?>">Retour</a>
+            </li>
+            <li class="nav-item d-flex align-items-center align">
+              <?php the_date(); ?>
+            </li>
+        </ul>
+    </div>
 
-<ul class="nav nav-pills ">
+    <div class="navbar-collapse collapse w-100 order-2 dual-collapse2">
+        <ol class="navbar-nav ml-auto">
+            <li class="nav-item " >
+            <?php the_category(); ?>
+            </li>
+           
+        </ol>
+    </div>
+</nav>
 
-<li class="border-right"><i class="fas fa-arrow-left"></i> <a href="<?php echo home_url( '/' ); ?>">Retour</a></li>
-<li><?php the_date(); ?></li>
-<li class="d-flex justify-content-end"><?php the_category(); ?></li>
-</ul>
-</div>
+
 
 <div class="card-body intro">
 <h1 class="card-title"><?php the_title(); ?></h1>
 <p class="card-text intro"> Cake donut oat cake. Chocolate bar pastry gingerbread dessert chocolate. Lollipop cheesecake cupcake. Halvah muffin donut chupa chups caramels. Tart lemon drops oat cake candy canes halvah sesame snaps tootsie roll jelly-o. Icing sweet jelly pastry sugar plum halvah chocolate. Chocolate cake cotton candy cotton candy gummi bears pie. Jelly-o icing bear claw tiramisu. Cake icing muffin cake.</p>
          
       <div class="post__content">
-        <img class ="img-intro"src="http://localhost/CMS-Restaurant-Pantera/cms-restaurant-panthera/wordpress/wp-content/uploads/2021/01/a-hot-bowl-of-soup-on-a-table-top.jpg" alt="">
-      <p class="card-text ingredient">
-<h2>Ingredients</h2>
+        
+      <img class ="img-intro"src="http://localhost/CMS-Restaurant-Pantera/cms-restaurant-panthera/wordpress/wp-content/uploads/2021/01/a-hot-bowl-of-soup-on-a-table-top.jpg" alt="">
+      <div class="grid-intro">
+<div class="ingredient">
+        <p class="card-text ">
+<h2 class="title-intro">Ingredients</h2>
 <p>For 4 peoples - preparation 30 min :</p>
 <p> • &nbsp;&nbsp;&nbsp;&nbsp; Pommes de terre à chair ferme : 500 gr </p>
 <p> • &nbsp;&nbsp;&nbsp;&nbsp; Haricots verts : 800 gr</p>
@@ -31,18 +49,20 @@
 <p> • &nbsp;&nbsp;&nbsp;&nbsp; Alumettes de lardons fumés : 250 gr</p>
 <p> • &nbsp;&nbsp;&nbsp;&nbsp; Beurre : 1 c.à soupe</p>
 <p> • &nbsp;&nbsp;&nbsp;&nbsp; Vinaigrette balsamique : 2 c.à soupe</p>
-  </p>  
+  </p>  </div>
 
-  <div class="d-flex flex-column bd-highlight mb-3">
-  <div class="p-2 bd-highlight"><a href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a></div>
-  <div class="p-2 bd-highlight"><a href="https://www.twitter.com"><i class="fab fa-twitter"></i></a></div>
-  <div class="p-2 bd-highlight"><a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a></div>
+  <div class="social-media d-flex flex-column bd-highlight mb-3">
+  <div class="p-2 bd-highlight"><a href="https://www.facebook.com"><i class="fab fa-facebook-f emote1"></i></a></div>
+  <div class="p-2 bd-highlight"><a href="https://www.twitter.com"><i class="fab fa-twitter emote2"></i></a></div>
+  <div class="p-2 bd-highlight"><a href="https://www.instagram.com"><i class="fab fa-instagram emote3"></i></a></div>
   <div class="p-2 bd-highlight"><a href="https://www.outlook.com"><i class="fas fa-envelope"></i></a></div>
+</div>
+
 </div>
   
   <div class="card-body instruction">
   <p class="card-text instruction">
-  <h2>Instruction</h2>
+  <h2 class="title-instruction">Instruction</h2>
 <p><span class="number">&nbsp;&nbsp;1&nbsp;&nbsp;</span> Powder soufflé jelly-o lemon drops cotton candy tart ice cream. Jujubes brownie icing macaroon jelly ice cream jelly beans cookie cookie. Jelly tootsie roll wafer.
 Tootsie roll bonbon cotton candy sweet roll powder marshmallow. Sugar plum sweet roll cheesecake. Fruitcake carrot cake tart sugar plum dessert.</p>
 </p>
