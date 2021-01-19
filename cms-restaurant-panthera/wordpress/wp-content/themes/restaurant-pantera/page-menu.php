5ac3f8606def25822d579c5d7d9f6cc908decb8d
iCarolinei <iCarolinei@github.com>
2021-01-19 15:03:02 +0100

    start menu

---
+++ cms-restaurant-panthera/wordpress/wp-content/themes/restaurant-pantera/page-menu.php
@@ -0,0 +1,62 @@
+<?php
+get_header() ?>
+<div class="bg-menu">
+<div class ="container menu">
+
+<div class="title-menu ">
+    <h2 class="d-flex justify-content-center">Welcome
+    </h2>
+    <h1 class="d-flex justify-content-center"> THE MENU</h1>
+</div>
+
+<div class="row">
+    <div class="col">
+
+        <div class="card-text border">
+       
+Cupcake ipsum dolor sit amet candy muffin chupa chups oat cake. Pudding halvah cake. Danish cheesecake sugar plum muffin chupa chups candy apple pie toffee halvah.
+
+Caramels gummies muffin gingerbread cake soufflé I love biscuit chocolate bar. Cake fruitcake gingerbread caramels marshmallow icing. Bonbon danish I love soufflé pudding. Tart lollipop brownie.
+
+Gummi bears oat cake lollipop toffee I love sweet pudding tiramisu. Sweet roll marshmallow dragée tootsie roll. Gingerbread liquorice liquorice cake bonbon I love I love muffin powder. Sesame snaps muffin pudding oat cake.
+
+Dragée danish lemon drops cake tiramisu macaroon oat cake. Sweet muffin chocolate topping. Soufflé I love I love chocolate bar wafer. Jujubes sugar plum I love.
+
+Dragée danish donut candy canes jelly beans marshmallow cookie marzipan pastry. Bonbon jujubes I love marshmallow. Sugar plum cake pastry gummi bears.
+    
+    </div>
+  
+
+    <div class="card-text border">
+       
+Cupcake ipsum dolor sit amet candy muffin chupa chups oat cake. Pudding halvah cake. Danish cheesecake sugar plum muffin chupa chups candy apple pie toffee halvah.
+
+Caramels gummies muffin gingerbread cake soufflé I love biscuit chocolate bar. Cake fruitcake gingerbread caramels marshmallow icing. Bonbon danish I love soufflé pudding. Tart lollipop brownie.
+
+Gummi bears oat cake lollipop toffee I love sweet pudding tiramisu. Sweet roll marshmallow dragée tootsie roll. Gingerbread liquorice liquorice cake bonbon I love I love muffin powder. Sesame snaps muffin pudding oat cake.
+
+Dragée danish lemon drops cake tiramisu macaroon oat cake. Sweet muffin chocolate topping. Soufflé I love I love chocolate bar wafer. Jujubes sugar plum I love.
+
+Dragée danish donut candy canes jelly beans marshmallow cookie marzipan pastry. Bonbon jujubes I love marshmallow. Sugar plum cake pastry gummi bears.
+    
+       </div>
+
+       <div class="card-text border">
+       
+    
+Cupcake ipsum dolor sit amet candy muffin chupa chups oat cake. Pudding halvah cake. Danish cheesecake sugar plum muffin chupa chups candy apple pie toffee halvah.
+
+Caramels gummies muffin gingerbread cake soufflé I love biscuit chocolate bar. Cake fruitcake gingerbread caramels marshmallow icing. Bonbon danish I love soufflé pudding. Tart lollipop brownie.
+
+Gummi bears oat cake lollipop toffee I love sweet pudding tiramisu. Sweet roll marshmallow dragée tootsie roll. Gingerbread liquorice liquorice cake bonbon I love I love muffin powder. Sesame snaps muffin pudding oat cake.
+
+Dragée danish lemon drops cake tiramisu macaroon oat cake. Sweet muffin chocolate topping. Soufflé I love I love chocolate bar wafer. Jujubes sugar plum I love.
+
+Dragée danish donut candy canes jelly beans marshmallow cookie marzipan pastry. Bonbon jujubes I love marshmallow. Sugar plum cake pastry gummi bears.
+       </div>
+
+        </div>
+        </div> 
+        </div>
+        </div> 
+<?php get_footer() ?>
