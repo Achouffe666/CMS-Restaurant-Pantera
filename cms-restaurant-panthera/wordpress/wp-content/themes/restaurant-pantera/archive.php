c5b333624018a6bc564d1d0486d63711b246bacb
iCarolinei <iCarolinei@github.com>
2021-01-20 09:42:11 +0100

    no idea

---
+++ cms-restaurant-panthera/wordpress/wp-content/themes/restaurant-pantera/archive.php
@@ -0,0 +1,108 @@
+<?php get_header(); ?>
+<div class="bg">
+
+<div class="container home">
+
+<div class="color">
+<?php $cuisines = get_terms(['category' => 'cuisine']); ?>
+
+<ul class="nav nav-pills my-6">
+    <?php foreach($cuisines as $cuisine): ?>
+		 <li class="nav-item d-flex justify-content-center">
+			 <img src="<?php echo get_template_directory_uri(); ?>/svg/cutelry.svg" alt="" style="width: 15px;margin-right: 5px;">
+		<a href="<?= get_term_link($cuisine) ?>" class="nav-link <?= is_tax('cuisine', $cuisine->term_id) ? 'active' : '' ?>"><?= $cuisine->name ?></a>
+		
+    </li>
+    <?php endforeach; ?>
+</ul></div>
+
+
+<?php
+echo 'PAGE archive.php';
+$term_id = get_queried_object_id();
+// $category = get_category( get_query_var( 'cat' ) );
+// $cat_id = getCurrentCatID();
+// $category = get_queried_object();
+// $term = get_query_var( 'term' );
+// echo 'PAGE archive.php   ';
+// echo '  CAT = ' . get_query_var('cat');
+// echo '   term = ' . $term;
+// echo '   term_id =  ' . $term_id ; 
+// //$the_query = new WP_Query( array( 'terms' => $term,'paged' => get_query_var( 'paged' ) ) );
+
+$args = array(
+    'post_type' => 'post',
+    'tax_query' => array(
+        array(
+        'taxonomy' => 'cuisine',
+        'field' => 'term_id',
+        'terms' => $term_id
+         )
+        )
+    
+    );
+    $the_query = new WP_Query( $args );
+$i=0; if( $the_query->have_posts() ) : while( $the_query->have_posts() ) : $the_query->the_post();
+
+	if ($i==0){ $i++;
+
+
+	?>
+
+    <div class="container col-xs-12 col-sm-12">
+    <div class="row border">
+	 <div class="col-7 reset"><?php the_post_thumbnail("post-thumbnail", ["class" => "card-img", "alt" => "", "style" => "height: auto;"
+    ]) ?> </div>
+	<div class="col-5 reset card-body">
+	<i class="far fa-clock"></i> <?php the_time( get_option( 'date_format' ) ); ?> 
+	</p>
+			<article class="post"> <img src="<?php echo get_template_directory_uri(); ?>/svg/cutelry.svg" alt="" style="width: 15px;margin-right: 5px;"><?php the_terms(get_the_ID(),"cuisine") ?>
+		<h5 class="card-title"><?php the_title(); ?></h5>
+            
+			<p class="card-text">
+			<?php the_excerpt(); ?>
+		<p class="d-flex justify-content-center">
+                <a href="<?php the_permalink(); ?>" class="post__link"><button type="button" class="btn btn-dark">Read more</button></a>
+            </p>
+		</article>
+  </div>
+</div>
+ </div>
+<?php } 
+else{?><div class="container col-xs-12 col-sm-12">
+    <div class="row border">
+	 
+	<div class="col-5 reset card-body">
+	<i class="far fa-clock"></i> <?php the_time( get_option( 'date_format' ) ); ?> 
+	</p>
+			<article class="post"> <img src="<?php echo get_template_directory_uri(); ?>/svg/cutelry.svg" alt="" style="width: 15px;margin-right: 5px;"><?php the_terms(get_the_ID(),"cuisine") ?>
+		<h5 class="card-title"><?php the_title(); ?></h5>
+            
+			<p class="card-text">
+			<?php the_excerpt(); ?>
+		<p class="d-flex justify-content-center">
+                <a href="<?php the_permalink(); ?>" class="post__link"><button type="button" class="btn btn-dark">Read more</button></a>
+            </p>
+		</article>
+  </div>
+  <div class="col-7 reset"><?php the_post_thumbnail("post-thumbnail", ["class" => "card-img", "alt" => "", "style" => "height: auto;"
+    ]) ?> </div>
+</div>
+ </div>
+<?php $i=0;
+}
+?>
+
+
+	<?php endwhile; endif; ?>
+	
+
+
+
+
+</div>
+
+<?php include("menuoverview.php"); ?></div>
+
+
+<?php get_footer(); ?>
